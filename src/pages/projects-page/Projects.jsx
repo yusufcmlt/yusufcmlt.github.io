6c86187021fb94ce113844f8d53312b93dda5a6f
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-11-11 23:24:54 +0300

    projects page design fix

--- src/pages/projects-page/Projects.jsx
+++ src/pages/projects-page/Projects.jsx
@@ -4,13 +4,21 @@ import projectsList from "../../utils/projects-list";
 import ProjectItem from "./project-item/ProjectItem";
 
 const Projects = () => {
+  const randomKey = () => {
+    return Math.round(Math.random() * 1000);
+  };
+
   return (
     <div className="projects-page-container">
       <h1>Some of My Work</h1>
-      <p>Some work i did while i was learning, freelancing etc. </p>
+      <p>
+        Some work i did while i was learning, freelancing etc.
+        <small className="small-text">(mostly learning.)</small>
+      </p>
       <div className="projects-container">
         {projectsList.map((project) => (
           <ProjectItem
+            key={randomKey()}
             title={project.title}
             image={project.image}
             text={project.text}
