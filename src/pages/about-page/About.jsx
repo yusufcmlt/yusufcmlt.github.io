f674017c27520fd922cd143e17cdefb223f8a2cc
PDOUU <58252790+pdouu@users.noreply.github.com>
2020-11-11 23:24:10 +0300

    changes for mobile design

--- src/pages/about-page/About.jsx
+++ src/pages/about-page/About.jsx
@@ -9,13 +9,13 @@ const About = () => {
   return (
     <div className="about-page-container">
       <h1>{aboutPageText.hello_message}</h1>
-
       <p className="description-p">My name is Yusuf Cemal.</p>
       <div className="description-p">
         I am a <TypingAnimation /> living in Turkey.
       </div>
       <p>I've used (or im using):</p>
       <div className="tools-image" />
+
       <Link to={`${process.env.PUBLIC_URL}/projects`} className="projects-link">
         in my projects...
       </Link>
