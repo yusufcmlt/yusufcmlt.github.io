13747a5be761b8676b609f7ef014803ce8d91ffc
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-06-03 15:43:22 +0300

    implement button with link

--- src/pages/about-page/skill-item/SkillItem.jsx
+++ src/pages/about-page/skill-item/SkillItem.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./SkillItem.style.scss";
 import "./SkillIcons.style.scss";
 
-export const SkillItem = ({ name }) => {
+export const SkillItem = ({ name, children, ...props }) => {
   return (
-    <div className="skill-item-container">
-      <span className={`skill-icon ${name ? `icon-${name}` : null}`} />
+    <div className="skill-item-container" {...props}>
+      {children}
+      <span
+        className={`skill-icon ${
+          name ? `icon-${name.toLowerCase().split(".").join("-")}` : null
+        }`}
+      />
       <span className="skill-text">{name}</span>
     </div>
   );
