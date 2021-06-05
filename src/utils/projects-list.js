088fae2dc5fe550e647d950ddb490e00f6f00fb4
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-06-06 01:13:39 +0300

    detail project descriptions

--- src/utils/projects-list.js
+++ src/utils/projects-list.js
@@ -10,14 +10,14 @@ import freecodecampVid from "../assets/project-vids/freecodecamp.mp4";
 
 const projectList = [
   {
-    title: "Guess The Drawing / Online Pictionary Game",
+    title: "Guess The Drawing / Multiplayer Pictionary Game",
     video: cizimOyunuVid,
     image: cizimOyunuImg,
     links: {
       github: "https://github.com/yusufcmlt/cizim-oyunu",
       web: "http://cizimoyunu.github.io",
     },
-    text: "A web based multiplayer drawing game with React, Socket.io and p5.js.",
+    text: "A web based multiplayer game that players try to guess each other's drawings. Players can create game rooms and others can join randomly on permission or with a key. I've used Socket.io for communication, React for UI, p5.js for drawing and Firebase Realtime Database for config database.",
     tags: ["React", "SocketIO", "Firebase", "Hooks", "p5.js"],
   },
   {
@@ -28,7 +28,7 @@ const projectList = [
       github: "https://github.com/yusufcmlt/react-ecommerce",
       web: "https://react-ecommerce-isyeri.web.app",
     },
-    text: "E-commerce app with React and Firebase.",
+    text: "E-commerce app that is selling nuts. Created with React and Firebase. Users can list products,categories, buy products, use cart and manage addresses. Users can also manage products and categories as an admin. I've used React Context Api and Hooks for managing app state, React Router for routing. On the backend; i've used Firebase Firestore for database management and Firebase Storage for file management.",
     tags: ["React", "Context", "Firebase", "Hooks"],
   },
   {
@@ -39,7 +39,7 @@ const projectList = [
       github: "https://github.com/yusufcmlt/nodemcu-home-project",
       web: "https://yusufcmlt.github.io/nodemcu-home-project/",
     },
-    text: "A smart home prototype with NodeMCU microcontroller, react and firebase as user interface.",
+    text: "A smart home prototype with NodeMCU microcontroller. On finished project with user interface, users can toggle leds,fans and view realtime informations,charts such as temperature, humidity, smoke data . Created with React,and Firebase Realtime Database.",
     tags: ["NodeMCU", "Firebase", "React", "Hooks"],
   },
   {
@@ -50,7 +50,7 @@ const projectList = [
       github: "https://github.com/yusufcmlt/freecodecamp-projects",
       web: "https://github.com/yusufcmlt/freecodecamp-projects",
     },
-    text: "Projects that i've done as a part of freeCodeCamp front-end developer curriculum. Pomodoro clock, calculator, drum machine, markdown editor and random quote apps.",
+    text: "Projects that i've done as a part of freeCodeCamp front-end libraries curriculum. Pomodoro clock, calculator, drum machine, markdown editor and random quote apps.",
     tags: ["React", "FreeCodeCamp", "Hooks"],
   },
 ];
