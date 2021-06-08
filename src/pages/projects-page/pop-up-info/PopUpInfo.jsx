aa279b6cf252f7e364e4a8355c1e07c1222457d6
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-06-08 11:44:18 +0300

    add loading

--- src/pages/projects-page/pop-up-info/PopUpInfo.jsx
+++ src/pages/projects-page/pop-up-info/PopUpInfo.jsx
@@ -8,6 +8,7 @@ export default function PopUpInfo({ projectIndex, handleProjectPopup }) {
   const { title, video, links, text, tags } = projectList[projectIndex];
 
   const [isMounted, setIsMounted] = useState(false);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     window.requestAnimationFrame(() => {
@@ -15,49 +16,59 @@ export default function PopUpInfo({ projectIndex, handleProjectPopup }) {
     });
   }, []);
 
+  function handleLoadEnd() {
+    console.log("Video Loaded");
+    setLoading(false);
+  }
+
   return (
     <>
       <div className="popup-background" onClick={() => handleProjectPopup()} />
       <div className={`popup-container ${isMounted ? "enter" : ""}`}>
         <button className="close-popup" onClick={() => handleProjectPopup()} />
         <div className="vid-container">
-          <video autoPlay loop muted>
+          <video autoPlay loop muted onLoadedData={handleLoadEnd}>
             <source src={video} type="video/mp4" />
           </video>
         </div>
-
         <div className="info-container">
-          <h2>{title}</h2>
-          <div className="links-container">
-            <SkillItem name="Github">
-              <a
-                className="link github-link"
-                href={links.github}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {}
-              </a>
-            </SkillItem>
-            <SkillItem name="Web">
-              <a
-                className="link web-link"
-                href={links.web}
-                target="_blank"
-                rel="noreferrer"
-              >
-                {}
-              </a>
-            </SkillItem>
-          </div>
-          <p>{text}</p>
-          <div className="tags-container">
-            {tags.map((tag, index) => (
-              <span key={index} className="tag">
-                {tag}
-              </span>
-            ))}
-          </div>
+          {isLoading ? (
+            <div className="loading" />
+          ) : (
+            <>
+              <h2>{title}</h2>
+              <div className="links-container">
+                <SkillItem name="Github">
+                  <a
+                    className="link github-link"
+                    href={links.github}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {}
+                  </a>
+                </SkillItem>
+                <SkillItem name="Web">
+                  <a
+                    className="link web-link"
+                    href={links.web}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {}
+                  </a>
+                </SkillItem>
+              </div>
+              <p>{text}</p>
+              <div className="tags-container">
+                {tags.map((tag, index) => (
+                  <span key={index} className="tag">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
